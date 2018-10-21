37634cbc3bb33690dcdb43d63c727386fdd64d9e
吕旭东 <1421199102@qq.com>
2018-10-21 13:57:18 +0800

    python学习之路

--- 1-python基础/2-OOP/01.py
+++ 1-python基础/2-OOP/01.py
@@ -1,15 +1,15 @@
 
-'''
-定以一个学生类，用来形容学生
-'''
-# 定义一个空的类
-class Student():
-    # 一个空类，pass代表直接跳过
-    # 此处pass必须有
-    pass
-
-# 定义一个对象
-mingyue = Student()
+# '''
+# 定以一个学生类，用来形容学生
+# '''
+# # 定义一个空的类
+# class Student():
+#     # 一个空类，pass代表直接跳过
+#     # 此处pass必须有
+#     pass
+#
+# # 定义一个对象
+# mingyue = Student()
 
 # 在定义一个类，用来描述听Python的学生
 class PythonStudent():
