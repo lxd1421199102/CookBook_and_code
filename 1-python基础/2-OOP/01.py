d48a4dcd40db595a0a64aefb94fa2b71887db03a
吕旭东 <1421199102@qq.com>
2018-10-21 14:07:15 +0800

    python学习

--- 1-python基础/2-OOP/01.py
+++ 1-python基础/2-OOP/01.py
@@ -1,16 +1,4 @@
 
-# '''
-# 定以一个学生类，用来形容学生
-# '''
-# # 定义一个空的类
-# class Student():
-#     # 一个空类，pass代表直接跳过
-#     # 此处pass必须有
-#     pass
-#
-# # 定义一个对象
-# mingyue = Student()
-
 # 在定义一个类，用来描述听Python的学生
 class PythonStudent():
     # 用None给不确定的值赋值
